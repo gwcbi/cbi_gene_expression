f4662ffc69b814904a16a61386edd0b18fa6afb7
Matthew Bendall <bendall@gwu.edu>
2016-08-30 08:29:48 -0400

    minimal setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='cbi_gene_expression',
+    description='CBI tools for analyzing gene expression',
+    url='https://github.com/gwcompbio/cbi_gene_expression',
+    author='Matthew L. Bendall',
+    version='0.1',
+    packages=['cbi_gene_expression',],
+)
